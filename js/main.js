a69ae5530119f673cba9a656ec76705beedd24b0
Elena <elenamitchenko@yandex.ru>
2020-09-30 21:18:53 +0300

    Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -67,3 +67,105 @@ for (let i = 0; i < adverts.length; i++) {
 }
 
 pinListElement.appendChild(fragment);
+
+let cardListElement = document.querySelector(`.map div:nth-child(1)`);
+let cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+
+function renderCard(advert) {
+  let cardElement = cardTemplate.cloneNode(true);
+
+  if (advert.author.avatar) {
+    cardElement.querySelector(`.popup__avatar`).src = advert.author.avatar;
+  } else {
+    cardElement.querySelector(`.popup__avatar`).remove();
+  }
+
+  if (advert.offer.title) {
+    cardElement.querySelector(`.popup__title`).innerHTML = advert.offer.title;
+  } else {
+    cardElement.querySelector(`.popup__title`).remove();
+  }
+
+  if (advert.offer.address) {
+    cardElement.querySelector(`.popup__text--address`).innerHTML = advert.offer.address;
+  } else {
+    cardElement.querySelector(`.popup__text--address`).remove();
+  }
+
+  if (advert.offer.price) {
+    cardElement.querySelector(`.popup__text--price`).innerHTML = advert.offer.price + `₽/ночь`;
+  } else {
+    cardElement.querySelector(`.popup__text--price`).remove();
+  }
+
+  if (advert.offer.type) {
+    switch (advert.offer.type) {
+      case `palace`:
+        cardElement.querySelector(`.popup__type`).innerHTML = `Дворец`;
+        break;
+      case `bungalow`:
+        cardElement.querySelector(`.popup__type`).innerHTML = `Дворец`;
+        break;
+      case `flat`:
+        cardElement.querySelector(`.popup__type`).innerHTML = `Квартира`;
+        break;
+      default:
+        cardElement.querySelector(`.popup__type`).innerHTML = `Дом`;
+    }
+  } else {
+    cardElement.querySelector(`.popup__type`).remove();
+  }
+
+  if (advert.offer.rooms && advert.offer.guests) {
+    cardElement.querySelector(`.popup__text--capacity`).innerHTML = advert.offer.rooms + ` комнаты для ` + advert.offer.guests + ` гостей`;
+  } else {
+    cardElement.querySelector(`.popup__text--capacity`).remove();
+  }
+
+  if (advert.offer.checkin && advert.offer.checkout) {
+    cardElement.querySelector(`.popup__text--time`).innerHTML = `Заезд после` + advert.offer.checkin + `, выезд до ` + advert.offer.checkout;
+  } else {
+    cardElement.querySelector(`.popup__text--time`).remove();
+  }
+
+  if (advert.offer.features) {
+    for (let i = 0; i < ARRAY_FEATURES.length; i++) {
+      if (!advert.offer.features.some(function (value) {
+        return value === ARRAY_FEATURES[i];
+      })) {
+        cardElement.querySelector(`.popup__features`).removeChild(cardElement.querySelector(`.popup__feature--` + ARRAY_FEATURES[i]));
+      }
+    }
+  } else {
+    cardElement.querySelector(`.popup__features`).remove();
+  }
+
+  if (advert.offer.description) {
+    cardElement.querySelector(`.popup__description`).innerHTML = advert.offer.description;
+  } else {
+    cardElement.querySelector(`.popup__description`).remove();
+  }
+
+  function renderPhoto(ad) {
+    let photo = cardElement.querySelector(`.popup__photo`).cloneNode(true);
+    photo.src = ad;
+
+    return photo;
+  }
+
+  if (advert.offer.photos) {
+    for (let i = 0; i < advert.offer.photos.length; i++) {
+      fragment.appendChild(renderPhoto(advert.offer.photos[i]));
+    }
+    cardElement.querySelector(`.popup__photos`).appendChild(fragment);
+    cardElement.querySelector(`.popup__photos`).removeChild(cardElement.querySelectorAll(`.popup__photo`)[0]);
+  } else {
+    cardElement.querySelector(`.popup__photos`).remove();
+  }
+
+  return cardElement;
+}
+
+let cardFragment = document.createDocumentFragment();
+cardFragment.appendChild(renderCard(adverts[0]));
+cardListElement.appendChild(cardFragment);
